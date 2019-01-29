8ad5a780bd6c5bb829d22285801cce700cda1d31
Takumi Adachi <takumiadachi@gmail.com>
2019-01-29 11:04:41 -0800

    Added styles to snackbars. Green for online, Yellow for offline

--- src/js/components/NetworkStatusSnackbar.jsx
+++ src/js/components/NetworkStatusSnackbar.jsx
@@ -1,8 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Snackbar from "@material-ui/core/Snackbar";
+import SnackbarContent from "@material-ui/core/SnackbarContent";
+import classNames from "classnames";
+import WarningIcon from "@material-ui/icons/Warning";
+import { withStyles } from "@material-ui/core/styles";
+import green from "@material-ui/core/colors/green";
+import amber from "@material-ui/core/colors/amber";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 
-export default class NetworkStatusSnackbar extends React.Component {
+const variantIcon = {
+  success: CheckCircleIcon,
+  warning: WarningIcon
+};
+
+const styles = theme => ({
+  success: {
+    backgroundColor: green[600]
+  },
+  warning: {
+    backgroundColor: amber[700]
+  },
+  icon: {
+    fontSize: 20
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit
+  },
+  message: {
+    display: "flex",
+    alignItems: "center"
+  }
+});
+
+function CustomSnackbarContent(props) {
+  const { classes, variant, message } = props;
+  const Icon = variantIcon[variant];
+  console.log(props);
+  return (
+    <SnackbarContent
+      className={classNames(classes[variant])}
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={classNames(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+    />
+  );
+}
+
+const CustomSnackbarContentWrapper = withStyles(styles)(CustomSnackbarContent);
+
+class NetworkStatusSnackbar extends React.Component {
   state = {
     open: true
   };
@@ -21,17 +72,22 @@ export default class NetworkStatusSnackbar extends React.Component {
     const { open } = this.state;
     const { status } = this.props;
 
+    let vari;
+    if (status === "Online") {
+      vari = "success";
+    } else {
+      vari = "warning";
+    }
+
     return (
       <div>
         <Snackbar
           open={open}
           onClose={this.handleClose.bind(this)}
-          ContentProps={{
-            "aria-describedby": "message-id"
-          }}
-          message={<span id="message-id">{status}</span>}
           transitionDuration={500}
-        />
+        >
+          <CustomSnackbarContentWrapper variant={vari} message={status} />
+        </Snackbar>
       </div>
     );
   }
@@ -41,3 +97,6 @@ NetworkStatusSnackbar.propTypes = {
   open: PropTypes.bool,
   status: PropTypes.string
 };
+
+export default NetworkStatusSnackbar;
+// export default NetworkStatusSnackbar;
